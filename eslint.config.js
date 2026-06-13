494aaaadc0a95ced4dbca19e6aa5aaea21023ae8
Harun Eggleton <haruneggleton@gmail.com>
2026-06-13 15:00:20 +1200

    rework ts

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off"
+        }
+    }
+);
